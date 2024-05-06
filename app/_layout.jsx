3a6501b235afdcb57e9782e5263fcdc7af3f665b
InesFrederickx <ines.frederickx@student.ehb.be>
2024-05-06 20:32:51 +0200

    Installed NativeWind and a test profile page
    - Installed NativeWind.
    
    - Added a profile page for link testing.
    
    - Adjusted babel.config.js and tailwind.config.js accordingly.

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,20 +1,12 @@
-import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { Slot, Stack } from "expo-router";
 
-export default function App() {
+const RootLayout = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Stack>
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+    </Stack>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+export default RootLayout;
