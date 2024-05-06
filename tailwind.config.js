5421100f84818bf340a4e8954585ccdb4984bf6a
InesFrederickx <ines.frederickx@student.ehb.be>
2024-05-06 22:16:00 +0200

    Added fonts, colors and folders for assets
    - Added the fonts in "tailwind.config.js" and in the folder "fonts" inside the "assets" folder.
    
    - Added the colours in "tailwind.config.js".
    
    - Added an "icons" and "images" folder inside the "assets" folder.
    
    - Added the "constants" folder with the needed JavaScript files for easier import of images and icons. Not yet coded.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,28 @@
 module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#FEFCFB",
+        "gradient-start": "#ffffff",
+        "gradient-end": "#FCEDE7",
+        secondary: {
+          DEFAULT: "#44402F",
+        },
+        pink: {
+          DEFAULT: "#FCEDE7",
+        },
+      },
+      fontFamily: {
+        avlightitalic: ["AveriaSerifLibre-LightItalic", "sans-serif"],
+        avlight: ["AveriaSerifLibre-Light", "sans-serif"],
+        avregular: ["AveriaSerifLibre-Regular", "sans-serif"],
+        avitalic: ["AveriaSerifLibre-Italic", "sans-serif"],
+        avbold: ["AveriaSerifLibre-Bold", "sans-serif"],
+        avbolditalic: ["AveriaSerifLibre-BoldItalic", "sans-serif"],
+        yesregular: ["YesevaOne-Regular", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
